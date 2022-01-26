58e832fd5f1923d0b134de7c500bf90a79184b5b
g4bor <gabor.tanacs@yahoo.com>
2022-01-26 20:32:42 +0100

    Extend UI pages

--- src/main/java/com/g4bor/payment/webui/controller/UIController.java
+++ src/main/java/com/g4bor/payment/webui/controller/UIController.java
@@ -3,6 +3,9 @@ package com.g4bor.payment.webui.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/")
@@ -13,8 +16,22 @@ public class UIController {
         return "login";
     }
 
+    @RequestMapping(method = RequestMethod.GET, value = "/403")
+    public ModelAndView accessDenied(Principal user) {
+        String username = user.getName();
+        if (null == username) {
+            username = "stranger";
+        }
+
+        ModelAndView model = new ModelAndView();
+        String message = String.format("Hi %s, you are not authorized to view this page!", username);
+        model.addObject("message", message);
+        model.setViewName("403");
+        return model;
+    }
+
     @RequestMapping(method = RequestMethod.GET, value = "/transfer")
     public String mainPage() {
-        return "index";
+        return "transfer";
     }
 }
